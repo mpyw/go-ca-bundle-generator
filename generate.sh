45b01b05f87b2745383dd5066c815f855e412b4e
mpyw <ryosuke_i_628@yahoo.co.jp>
2019-11-10 18:36:06 +0900

    Formatting

--- generate.sh
+++ generate.sh
@@ -3,55 +3,55 @@
 data=$(curl https://curl.haxx.se/ca/cacert.pem)
 
 if [ ! -e cabundle ]; then
-    mkdir cabundle
+	mkdir cabundle
 fi
 
 cat << _EOT_ > cabundle/cabundle.go
 package cabundle
 
 import (
-    "unsafe"
-    "reflect"
-    "net/http"
-    "crypto/x509"
-    "crypto/tls"
+	"unsafe"
+	"reflect"
+	"net/http"
+	"crypto/x509"
+	"crypto/tls"
 )
 
 var strData = \`$data\`
 
 func pemData() []byte {
-    var empty [0]byte
-    sx := (*reflect.StringHeader)(unsafe.Pointer(&strData))
-    b := empty[:]
-    bx := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-    bx.Data = sx.Data
-    bx.Len = len(strData)
-    bx.Cap = bx.Len
-    return b
+	var empty [0]byte
+	sx := (*reflect.StringHeader)(unsafe.Pointer(&strData))
+	b := empty[:]
+	bx := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	bx.Data = sx.Data
+	bx.Len = len(strData)
+	bx.Cap = bx.Len
+	return b
 }
 
 func GetCertPool() *x509.CertPool {
-    certs := x509.NewCertPool()
-    certs.AppendCertsFromPEM(pemData())
-    return certs
+	certs := x509.NewCertPool()
+	certs.AppendCertsFromPEM(pemData())
+	return certs
 }
 
 func GetTlsConfig() *tls.Config {
-    return &tls.Config {
-        RootCAs: GetCertPool(),
-    }
+	return &tls.Config {
+		RootCAs: GetCertPool(),
+	}
 }
 
 func GetTransport() *http.Transport {
-    return &http.Transport {
-        TLSClientConfig: GetTlsConfig(),
-    }
+	return &http.Transport {
+		TLSClientConfig: GetTlsConfig(),
+	}
 }
 
 func GetClient() *http.Client {
-    return &http.Client {
-        Transport: GetTransport(),
-    }
+	return &http.Client {
+		Transport: GetTransport(),
+	}
 }
 
 _EOT_
--- test.go
+++ test.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "io/ioutil"
-    "./cabundle"
+	"./cabundle"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-    req, _ := http.NewRequest("GET", "https://api.twitter.com/1.1/dummy.json", nil)
-    resp, err := cabundle.GetClient().Do(req)
-    if err != nil {
-        log.Fatalln(err)
-    } else {
-        defer resp.Body.Close()
-        bytes, _ := ioutil.ReadAll(resp.Body)
-        fmt.Println(string(bytes))
-        log.Println("TLS negotiation successful")
-    }
+	req, _ := http.NewRequest("GET", "https://api.twitter.com/1.1/dummy.json", nil)
+	resp, err := cabundle.GetClient().Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	} else {
+		defer resp.Body.Close()
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(bytes))
+		log.Println("TLS negotiation successful")
+	}
 }
